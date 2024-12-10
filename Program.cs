68276a86e1ff039c5e3704ce0ca9c6a9b2f31bb1
aysesogut03 <aysesogut2003@gmail.com>
2024-12-10 10:13:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz");
+            int girilenSayi=Convert.ToInt32(Console.ReadLine());
+            tekmiciftmi(girilenSayi);
+            Console.ReadLine();
+        }
+
+        static void tekmiciftmi(int girilenSayi)
+        {
+            if(girilenSayi%2 == 0)
+            {
+                Console.WriteLine("Girilen sayı çift sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı tek sayıdır");
+            }
+        }
+    }
+}
